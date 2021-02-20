c387880bf3b09e9a4244da88e13dde6d4fa1c0f8
DESKTOP-5VPV7SS\oluad <OLUADESINA87@HOTMAIL.COM>
2021-02-20 21:03:31 +0000

    added toolbar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 
 import React, { Component } from 'react';
 import Logo from '../../Assets/burger-logo.png';
+import classes from '../Logo/Logo.module.css';
 
 
 const logo = (props) => (
 
-    <div>
-        <image  alt='Burgers R us'/>
+    <div className={classes.Logo}>
+        <image src={Logo}  alt='Burgers R us'/>
     </div>
 
 )
