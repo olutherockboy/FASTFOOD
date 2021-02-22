7a62f59da67a43e5331cd1c0b55074aaeb323c0a
DESKTOP-5VPV7SS\oluad <OLUADESINA87@HOTMAIL.COM>
2021-02-22 22:06:07 +0000

    logo sorted

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,14 @@
 
 import React, { Component } from 'react';
-import Logo from '../../Assets/burger-logo.png';
+import logoo from '../../Assets/burger-logo.png';
 import classes from '../Logo/Logo.module.css';
+import rEA from '../../Assets/logo192.png';
 
 
 const logo = (props) => (
 
     <div className={classes.Logo}>
-        <image src={Logo}  alt='Burgers R us'/>
+        <img src={logoo}  alt='Burgers R us'/>
     </div>
 
 )
